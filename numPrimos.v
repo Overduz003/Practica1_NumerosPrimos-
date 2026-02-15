d501aeff90730673f18305bcb6dc8c905390f7ce
Overduz003 <A07019516@tec.mx>
2026-02-14 20:16:40 -0600

    Update numPrimos.v

--- numPrimos.v
+++ numPrimos.v
@@ -1,10 +1,16 @@
 module numPrimos (
 	
-	input [3:0] S,
-	output led);
+	input [3:0] S, // entrada de los switches
+	output led);  // Salida led 
 	
-	//d~(cb)+~d(ca)+~(da) formula
-	
+	//Utilice el metodo de Structure Style
+	//Con un tabla de verdad y un mapa de karnaught obtuve una formula para obtener los numeros primos
+
+	//Formula -->  ~(~(~d ~c b ) & ~(~d c a ) & ~( c ~b a) & ~(~c b a ) 
+	//Puse una imagen que muestra lo que hice "Tabla_Formula"
+
+	//Formula de la salida del led 
 	assign led = ~(~(~S[3] & ~S[2] & S[1]) & ~(~S[3] & S[2] & S[0]) & ~(S[2] & ~S[1] & S[0]) & ~(~S[2] & S[1] & S[0]));
 						
+
 endmodule
